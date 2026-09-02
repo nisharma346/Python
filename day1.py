42c2ee0731c18f7959930819378f5072665dc9f7
nisharma346 <nisharma346@gmail.com>
2026-09-02 12:23:11 +0530

    add

--- day1.py
+++ day1.py
@@ -5,4 +5,86 @@
 # Python was created by Guido van Rossum and was first released in 1991.
 
 # why python is called a high level language?
-# Python is called a high-level programming language because its syntax is easy for humans to read and understand and it hides most of the complex details of the computer's hardware.
+# Python is called a high-level programming language because its syntax is easy for humans
+#  to read and understand and it hides most of the complex details of the computer's hardware.
+
+# Five Main Features of Python
+
+# 1.Simple and Easy to Learn
+# Python has a simple syntax that is easy for beginners to understand and write.
+
+# 2.Language
+# Python code is executed by an interpreter, so it does not need to be compiled separately before execution.
+
+# 3.Object-Oriented
+# Python supports object-oriented programming concepts such as classes, objects, inheritance, and polymorphism.
+
+# 4.Portable
+# Python programs can run on different operating systems such as Windows, Linux, and macOS with little or no modification.
+
+# 5.Large Standard Library
+# Python provides many built-in modules and libraries for tasks such as file handling, mathematics, networking, web development, and data processing.
+
+# why python is called an interpreted language?
+# Python is called an interpreted language because Python programs are executed by an interpreter, rather than being directly converted into machine 
+# code by the programmer before execution.
+
+# Applications of Python 🐍
+# 1. Web Development 🌐
+# Python is used to create websites and web applications using frameworks such as Django and Flask.
+# Example: E-learning websites, shopping websites, banking applications.
+
+# 2. Artificial Intelligence & Machine Learning 🤖
+# Python is widely used for AI and Machine Learning because of libraries such as NumPy, Pandas, TensorFlow, and Scikit-learn.
+# Example: Chatbots, recommendation systems, image recognition.
+
+# 3. Data Science & Data Analysis 📊
+# Python is used to collect, process, analyze, and visualize large amounts of data.
+# Example: Business reports, data visualization, prediction.
+
+# 4. Automation ⚙️
+# Python can automate repetitive tasks.
+# Example: Sending emails, managing files, generating reports, web automation.
+
+# 5. Game Development 🎮
+# Python can be used to develop simple games using libraries such as Pygame.
+
+# 6. Desktop Applications 🖥️
+# Python can be used to create desktop GUI applications using Tkinter, PyQt, etc.
+
+# 7. Scientific Computing 🔬
+# Python is used in scientific and mathematical applications for calculations, simulations, and research.
+
+# 8. Cybersecurity 🔐
+# Python is used for security testing, network analysis, and developing cybersecurity tools.
+
+# who develop python.
+
+# Python was developed by Guido van Rossum, a Dutch programmer.
+# 👨‍💻 Developer: Guido van Rossum
+# 📅 First released: 1991
+# 🇳🇱 Country: Netherlands
+# 🐍 Name: Python was named after the British comedy group Monty Python, not the snake.
+
+# Basic Difference Between Python and C
+# Feature	            Python	c Level	High-level language	                Middle-level language
+# Syntax	            Simple and easy	                                    More complex
+# Execution	            Interpreted (with bytecode + VM)	                Compiled
+# Speed	                Generally slower	                                Generally faster
+# Memory Management	    Automatic	                                        Mostly manual
+# Programming Style	    Supports OOP and multiple paradigms	                Mainly procedural
+# Pointers	            No direct pointer manipulation	                    Supports pointers
+# Use	                AI, ML, web development, data science, automation	Operating systems, embedded systems, system programming
+
+# Basic Difference Between Python and Java
+# Feature	               Python 🐍	                         Java ☕
+# Level	                   High-level language	                  High-level language
+# Syntax	               Simple and easy to read	              More structured and verbose
+# Execution	               Interpreted through Python runtime	  Compiled to bytecode and run by JVM
+# Typing	               Dynamically typed	                  Statically typed
+# Speed	                   Generally slower	                      Generally faster
+# Memory Management	       Automatic garbage collection   	      Automatic garbage collection
+# Code Length	           Usually shorter	                      Usually longer
+# Main Uses	               AI, ML, data science, automation,      Android, enterprise software, web applications, 
+#                          web development	                                       desktop applications
+#                          
--- map_function.py
+++
@@ -1,4 +0,0 @@
-import math
-lst=[3,5,6]
-fact_value=[math.factorial(x)for x in lst]
-print(fact_value)
