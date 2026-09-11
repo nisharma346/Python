35b2fd29652300bd0cc7cd88ab5bc9295dc8abad
nisharma346 <nisharma346@gmail.com>
2026-09-12 01:52:59 +0530

    add

---
+++ day2.py
@@ -0,0 +1,15 @@
+# print("hello world")
+# print("Name:Nisha")
+# print("Age:20")
+# print("City:samatipur")
+# name="Nisha"
+# age=20
+# city="samatipur"
+# print(name,age,city)
+python=80
+java=30
+dbms=40
+total=python+java+dbms
+print("Total=",total)
+average=total/3
+print("average=",average)
