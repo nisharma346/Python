c0959c29042c3634ab47085ed679a85c47081ead
nisharma346 <nisharma346@gmail.com>
2026-09-12 02:04:26 +0530

    add

--- day2.py
+++ day2.py
@@ -1,15 +1,32 @@
 # print("hello world")
+
 # print("Name:Nisha")
 # print("Age:20")
 # print("City:samatipur")
+
 # name="Nisha"
 # age=20
 # city="samatipur"
 # print(name,age,city)
-python=80
-java=30
-dbms=40
-total=python+java+dbms
-print("Total=",total)
-average=total/3
-print("average=",average)
+
+# python=80
+# java=30
+# dbms=40
+# total=python+java+dbms
+# print("Total=",total)
+# average=total/3
+# print("average=",average)
+
+# PI=3.14
+# r=6
+# area=PI*r*r
+# print(area)
+
+# age=20
+# print(type(age))
+
+# price=347.789
+# print(type(price))
+
+name="nisha"
+print(type(name))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-print("hello world")
+python=80
+java=30
+dbms=40
+total=python+java+dbms
+print("Total=",total)
+average=total/3
+print("average=",average)
