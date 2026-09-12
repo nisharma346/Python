95823f7ee42109902421a3b0301725650e56fced
nisharma346 <nisharma346@gmail.com>
2026-09-12 15:01:23 +0530

    add

--- day2.py
+++ day2.py
@@ -28,5 +28,36 @@
 # price=347.789
 # print(type(price))
 
-name="nisha"
-print(type(name))
+# name="nisha"
+# print(type(name))
+
+# is_student=True
+# print(type(is_student))
+
+# num=2+3j
+# print(type(num))
+
+# subjects=["python","dbms","java"]
+# print(subjects)
+# print(type(subjects))
+
+# numbers=(1,4,6)
+# print(numbers)
+# print(type(numbers))
+
+# numbers={1,4,6}
+# print(numbers)
+# print(type(numbers))
+
+# student = {
+#     "Name":"Nisha",
+#     "Age":23,
+#     "marks":80
+# }
+# print(student)
+# print(type(student))
+
+# num="100"
+# num=int(num)
+# print(num)
+# print(type(num))
